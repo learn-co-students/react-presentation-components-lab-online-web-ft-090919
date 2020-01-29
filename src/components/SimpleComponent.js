5937565db8c3a9da60eab0abf68412d420078e12
Doug Kintop <dkintop@hotmail.com>
2020-01-29 12:00:18 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -9,12 +9,12 @@ export default class SimpleComponent extends Component {
     };
   }
 
-  render() {
-    return <div onClick={this.handleClick()}>{this.state.mood}</div>;
-  }
-
   handleClick = () => {
     const newMood = this.state.mood === "happy" ? "sad" : "happy";
     this.setState({ mood: newMood });
   };
+
+  render() {
+    return <div onClick={this.handleClick()}>{this.state.mood}</div>;
+  }
 }
