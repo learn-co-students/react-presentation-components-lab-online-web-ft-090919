cd30a8c5485d0a04e8d7bbd21b7af801208120aa
mmman64 <mcnultymarco@gmail.com>
2020-02-01 20:34:09 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+handleClick = () => {
+  alert("Hello!")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.handleClick} />
   </div>,
   document.getElementById('root')
 );
